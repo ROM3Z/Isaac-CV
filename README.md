64ee0538cabafd2a60295b453b3f7fa3febd83c6
Isaac Rodriguez <isaac.rod33@gmail.com>
2021-10-11 13:02:46 -0400

    Desarrollo

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# IsaacCV
+# Isaac_RodriguezCV
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.11.
 
---
+++ src/app/blog/blog.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { CvabastidasService } from '../services/cvabastidas.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-blog',
+  templateUrl: './blog.component.html',
+  styleUrls: ['./blog.component.css']
+})
+export class BlogComponent implements OnInit {
+
+  constructor(public cv: CvabastidasService,
+              private sanitizer: DomSanitizer
+              ) { }
+
+  ngOnInit(): void {
+  }
+
+  getVideoIframe(url) {
+    let video;
+    let results;
+
+    if (url === null) {
+        return '';
+    }
+    results = url.match('[\\?&]v=([^&#]*)');
+    video = (results === null) ? url : results[1];
+
+    return this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + video);
+  }
+
+}
